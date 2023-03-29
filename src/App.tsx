6e5aaeb51a07444962b641af3e29d43fd74dcd3e
MagicHacker <zxmagichacker@gmail.com>
2023-03-29 15:30:50 +0800

    feat: 配置router路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import logo from './assets/svg/logo.svg';
+import { HashRouter } from 'react-router-dom';
+import Routers from './router';
 import './App.scss';
 
 function App() {
@@ -7,17 +9,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <HashRouter>
+          <Routers />
+        </HashRouter>
       </header>
     </div>
   );
