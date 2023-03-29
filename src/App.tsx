aa644b1a0f2f68be8042bdc03cd716c726fdcf38
MagicHacker <zxmagichacker@gmail.com>
2023-03-29 17:11:52 +0800

    feat: 删除App中的scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import routers from './router';
 import { useRoutes } from 'react-router-dom';
-import './App.scss';
 
 function App() {
   const routerElement = useRoutes(routers);
