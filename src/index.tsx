83bce33fa234db579a892816f7ff529b0b493ce3
MagicHacker <zxmagichacker@gmail.com>
2023-03-29 15:50:48 +0800

    feat: 配置antd

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
+import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
+import zhCN from 'antd/locale/zh_CN';
+import 'antd/dist/reset.css';
 import './index.scss';
 import App from './App';
+
+dayjs.locale('zh-cn');
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider locale={zhCN}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </ConfigProvider>
   </React.StrictMode>
 );
