d4faeaaf2c9b4c849a3ec2db99a2062e085406f7
MagicHacker <zxmagichacker@gmail.com>
2023-03-29 22:01:51 +0800

    feat: App组件添加类型

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import routers from './router';
 import { useRoutes } from 'react-router-dom';
 
-function App() {
+const App: React.FC = () => {
   const routerElement = useRoutes(routers);
   return (
     <>
       {routerElement}
     </>
   );
-}
+};
 
 export default App;
