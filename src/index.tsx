93c1692e9a9a0ae1ddd6caa6cf53d28145dac8f5
MagicHacker <zxmagichacker@gmail.com>
2023-03-30 10:47:08 +0800

    feat: mobx模块化，封装useStore

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
-import { Provider } from 'mobx-react';
 import { ConfigProvider } from 'antd';
 import dayjs from 'dayjs';
 import zhCN from 'antd/locale/zh_CN';
 import App from './App';
-import { stores } from './store/index';
 import 'antd/dist/reset.css';
 import './index.scss';
 import 'dayjs/locale/zh-cn';
@@ -19,11 +17,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ConfigProvider locale={zhCN}>
-      <Provider {...stores}>
-        <HashRouter>
-          <App />
-        </HashRouter>
-      </Provider>
+      <HashRouter>
+        <App />
+      </HashRouter>
     </ConfigProvider>
   </React.StrictMode>
 );
