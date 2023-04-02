662fbcc8e8faee032af4cddede01fd380366e874
MagicHacker <zxmagichacker@gmail.com>
2023-04-02 22:41:01 +0800

    feat: 构建主页组件

---
+++ src/views/HomePage/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './index.scss';
+const HomePage = (): JSX.Element => {
+  return (
+    <div className='homepage-wrapper'>HomePage</div>
+  );
+};
+
+export default HomePage;
