cbec20f12e331e086d83f7bcc34f7d73a739dca0
MagicHacker <zxmagichacker@gmail.com>
2023-04-03 16:41:43 +0800

    feat: 重定向路由

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,13 +1,18 @@
 // 声明式路由
 import React from 'react';
-import { RouteObject } from 'react-router-dom';
+import { RouteObject, Navigate } from 'react-router-dom';
 import HomePage from '../views/HomePage';
 
 const routers: RouteObject[] = [
   {
-    path: '/',
+    path: '/homepage',
     element: <HomePage />,
     children: []
-  }
+  },
+  // 重定向路由
+  {
+    path: '/',
+    element: <Navigate to="/homepage" />
+  },
 ];
 export default routers;
