c71c12012c3769208dc67ba350bc5dbf5f18dd47
MagicHacker <zxmagichacker@gmail.com>
2023-04-03 15:29:42 +0800

    feat: 构建首页布局

--- src/views/HomePage/index.tsx
+++ src/views/HomePage/index.tsx
@@ -1,8 +1,29 @@
 import React from 'react';
+import { Layout, Space } from 'antd';
+import Logo from '../../assets/img/logo.png';
 import './index.scss';
+
+const { Header, Sider, Content } = Layout;
 const HomePage = (): JSX.Element => {
   return (
-    <div className='homepage-wrapper'>HomePage</div>
+    <div className='homepage-wrapper'>
+      <Layout>
+        <Space>
+          <Sider className='homepage-sider'>
+            <div className='homepage-logo'>
+              <img src={Logo} alt="logo" />
+              <span>Ad-Admin</span>
+            </div>
+          </Sider>
+          <Layout>
+            <Space direction="vertical">
+              <Header className='homepage-header'>header</Header>
+              <Content>content</Content>
+            </Space>
+          </Layout>
+        </Space>
+      </Layout>
+    </div>
   );
 };
 
