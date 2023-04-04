2988a642fdea06353332d9d93b750664cc42092c
MagicHacker <zxmagichacker@gmail.com>
2023-04-04 16:18:40 +0800

    feat: 替换路径别名

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,15 +1,15 @@
 // 声明式路由
 import React from 'react';
 import { RouteObject, Navigate } from 'react-router-dom';
-import HomePage from '../views/HomePage';
-import Dashboard from '../views/Dashboard';
-import Selection from '../views/Selection';
-import AdPlatform from '../views/AdPlatform';
-import BuryPoint from '../views/BuryPoint';
-import Package from '../views/Package';
-import UserPortrait from '../views/UserPortrait';
-import Alternative from '../views/Alternative';
-import AutoTest from '../views/AutoTest';
+import HomePage from '@views/HomePage';
+import Dashboard from '@views/Dashboard';
+import Selection from '@views/Selection';
+import AdPlatform from '@views/AdPlatform';
+import BuryPoint from '@views/BuryPoint';
+import Package from '@views/Package';
+import UserPortrait from '@views/UserPortrait';
+import Alternative from '@views/Alternative';
+import AutoTest from '@views/AutoTest';
 
 const routers: RouteObject[] = [
   {
