493832d1ad6d312ddb35198ca16142bbaf523fed
MagicHacker <zxmagichacker@gmail.com>
2023-04-05 21:31:48 +0800

    feat: 编写主页card

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -1,7 +1,64 @@
 import React from 'react';
+import { Row, Col } from 'antd';
+import './index.scss';
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div className='dashboard-wrapper'>
+      <Row justify="space-between">
+        <Col className='card-item'>
+          <div className='card-info'>
+            <div>今日注册人数:</div>
+            <div className='info-data'>
+              <div>3428</div>
+              <div className='data-rise'>+3.4%</div>
+            </div>
+            <div>昨日注册:40人</div>
+          </div>
+          <div>1</div>
+        </Col>
+        <Col className='card-item'>
+          <div className='card-info'>
+            <div>今日DAU:</div>
+            <div className='info-data'>
+              <div>3428</div>
+              <div className='data-fall'>-3.4%</div>
+            </div>
+            <div>昨日DAU:400</div>
+          </div>
+          <div>2</div>
+        </Col>
+        <Col className='card-item'>
+          <div className='card-info'>
+            <div>今日收益:</div>
+            <div className='info-data'>
+              <div>3428</div>
+              <div className='data-rise'>+3.4%</div>
+            </div>
+            <div>昨日收益:40</div>
+          </div>
+          <div>3</div>
+        </Col>
+        <Col className='card-item'>
+          <div className='card-info'>
+            <div>今日注册人数:</div>
+            <div className='info-data'>
+              <div>3428</div>
+              <div className='data-fall'>-3.4%</div>
+            </div>
+            <div>昨日注册:30人</div>
+          </div>
+          <div>4</div>
+        </Col>
+      </Row>
+      <div>
+        <div>App月度注册统计表</div>
+        <div>小程序月度注册统计表</div>
+      </div>
+      <div>
+        <div>广告数据表格</div>
+        <div>用户画像列表</div>
+      </div>
+    </div>
   );
 }
