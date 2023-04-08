aa9f65ae6e65ee79fccb526b2fa989d9ab4279db
MagicHacker <zxmagichacker@gmail.com>
2023-04-08 22:57:38 +0800

    feat: 构建表格

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -4,11 +4,57 @@ import { LineChart, LineSeriesOption } from 'echarts/charts';
 import { GridComponent, TitleComponent, TitleComponentOption, GridComponentOption, TooltipComponent, TooltipComponentOption } from 'echarts/components';
 import { LabelLayout, UniversalTransition } from 'echarts/features';
 import { CanvasRenderer } from 'echarts/renderers';
-import { Row, Col, Progress } from 'antd';
+import { Row, Col, Progress, Table } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
 import './index.scss';
+interface DataType {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+}
 
 type EchartsOption = echarts.ComposeOption<TitleComponentOption | GridComponentOption | LineSeriesOption | TooltipComponentOption>
 export default function Dashboard() {
+  // 表格表头
+  const columns: ColumnsType<DataType> = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    }
+  ];
+  // 表格数据
+  const data: DataType[] = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sydney No. 1 Lake Park',
+    },
+  ];
   // 注册组件
   echarts.use([
     GridComponent,
@@ -148,10 +194,15 @@ export default function Dashboard() {
           </Col>
         </Row>
       </div>
-      <div>
-        <div>广告数据表格</div>
-        <div>用户画像列表</div>
-      </div>
+      <Row justify="space-between">
+        <Col className='dashborad-table'>
+          <div className='table-title'>广告数据</div>
+          <Table pagination={false} bordered={true} columns={columns} dataSource={data}/>
+        </Col>
+        <Col className='dashborad-list'>
+          <div className='list-title'>用户画像</div>
+        </Col>
+      </Row>
     </div>
   );
 }
