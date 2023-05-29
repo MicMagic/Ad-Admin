cef9e20ac28159f9b170b13f07ec8b84dd34d4a8
MagicHacker <zxmagichacker@gmail.com>
2023-05-29 21:50:08 +0800

    页面刷新菜单高亮

--- src/views/HomePage/index.tsx
+++ src/views/HomePage/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Layout, Space, Menu, MenuProps } from 'antd';
 import { observer } from 'mobx-react';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 import SvgIcon from '@components/SvgIcon/index';
 import MainHeader from '@components/MainHeader';
 import useStore from '@src/store';
@@ -9,8 +9,11 @@ import './index.scss';
 
 const { Header, Sider, Content } = Layout;
 const HomePage = (): JSX.Element => {
+  const [ defaultKey, setDefaultKey ] = useState('');
   const { commonStore } = useStore();
   const { menuCollapsed } = commonStore;
+  const { pathname } = useLocation();
+  
   // 菜单item类型别名
   type MenuItem = Required<MenuProps>['items'][number];
   const getItem = (
@@ -28,15 +31,19 @@ const HomePage = (): JSX.Element => {
   };
   // 配置路由菜单导航
   const items: MenuProps['items'] = [
-    getItem(<Link to="dashboard">dashboard</Link>, '1', <SvgIcon iconName='dashboard' />),
-    getItem(<Link to="selection">选品管理</Link>, '2', <SvgIcon iconName='selection' />),
-    getItem(<Link to="adPlatform">广告平台</Link>, '3', <SvgIcon iconName='adPlatform' />),
-    getItem(<Link to="buryPoint">埋点管理</Link>, '4', <SvgIcon iconName='buryPoint' />),
-    getItem(<Link to="package">包管理</Link>, '5', <SvgIcon iconName='package' />),
-    getItem(<Link to="userPortrait">用户画像</Link>, '6', <SvgIcon iconName='userPortrait' />),
-    getItem(<Link to="alternative">备选产品</Link>, '7', <SvgIcon iconName='alternative' />),
-    getItem(<Link to="autoTest">自动化测试</Link>, '8', <SvgIcon iconName='autoTest' />),
+    getItem(<Link to="dashboard">dashboard</Link>, 'dashboard', <SvgIcon iconName='dashboard' />),
+    getItem(<Link to="selection">选品管理</Link>, 'selection', <SvgIcon iconName='selection' />),
+    getItem(<Link to="adPlatform">广告平台</Link>, 'adPlatform', <SvgIcon iconName='adPlatform' />),
+    getItem(<Link to="buryPoint">埋点管理</Link>, 'buryPoint', <SvgIcon iconName='buryPoint' />),
+    getItem(<Link to="package">包管理</Link>, 'package', <SvgIcon iconName='package' />),
+    getItem(<Link to="userPortrait">用户画像</Link>, 'userPortrait', <SvgIcon iconName='userPortrait' />),
+    getItem(<Link to="alternative">备选产品</Link>, 'alternative', <SvgIcon iconName='alternative' />),
+    getItem(<Link to="autoTest">自动化测试</Link>, 'autoTest', <SvgIcon iconName='autoTest' />),
   ];
+  useEffect(() => {
+    const pathArr = pathname.split('/');
+    setDefaultKey(pathArr[pathArr.length - 1]);
+  }, [ pathname ]);
   return (
     <div className='homepage-wrapper'>
       <Layout>
@@ -49,7 +56,7 @@ const HomePage = (): JSX.Element => {
             <Menu
               mode="inline"
               items={items}
-              defaultSelectedKeys={[ '1' ]}
+              selectedKeys={[ `${defaultKey}` ]}
             />
           </Sider>
           <Layout>
