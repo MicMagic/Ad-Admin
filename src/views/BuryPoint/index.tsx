d9a43539b9f8d80b59f549aedb8ee98a8bb3342b
MagicHacker <zxmagichacker@gmail.com>
2023-06-04 17:51:55 +0800

    feat: 编写埋点header

--- src/views/BuryPoint/index.tsx
+++ src/views/BuryPoint/index.tsx
@@ -1,7 +1,54 @@
 import React from 'react';
+import { Form, Input, Button, Space, Dropdown, type MenuProps } from 'antd';
+import SvgIcon from '@components/SvgIcon/index';
+import { observer } from 'mobx-react';
+import './index.scss';
 
-export default function BuryPoint() {
+const BuryPoint = (): JSX.Element => {
+  const items: MenuProps['items'] = [
+    {
+      key: '1',
+      label: (
+        <Space>
+          <SvgIcon iconName='downloadtpl'/>
+          模板下载
+        </Space>
+      )
+    },
+    {
+      key: '2',
+      label: (
+        <Space>
+          <SvgIcon iconName='export'/>
+          文件导入
+        </Space>
+      )
+    }
+  ];
   return (
-    <div>埋点</div>
+    <div className='bury-point-wrapper'>
+      <div className='bury-point-header'>
+        <Form layout='inline'>
+          <Form.Item label='项目'>
+            <Input placeholder='请输入……'/>
+          </Form.Item>
+          <Form.Item label='应用类型'>
+            <Input placeholder='请输入……'/>
+          </Form.Item>
+          <Form.Item>
+            <Button type='primary'>搜索</Button>
+          </Form.Item>
+        </Form>
+        <Space>
+          <Button type='primary' icon={<SvgIcon iconName='new'/>}>新增</Button>
+          <Dropdown menu={{ items }} placement="bottom" arrow>
+            <Button icon={<SvgIcon iconName='dropdown'/>}>操作</Button>
+          </Dropdown>
+        </Space>
+      </div>
+      <div className=''></div>
+    </div>
   );
-}
+};
+
+export default observer(BuryPoint);
