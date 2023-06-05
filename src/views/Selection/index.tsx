69593678de73a9310be93f74f2e9be45f9002a6a
MagicHacker <zxmagichacker@gmail.com>
2023-06-05 22:25:34 +0800

    feat: 构建选品

--- src/views/Selection/index.tsx
+++ src/views/Selection/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Form, Input } from 'antd';
+import './index.scss';
 
 export default function Selection() {
   return (
-    <div>选品</div>
+    <div className='selection-wrapper'>
+      <div className='selection-header'>
+        <Form layout='inline'>
+          <Form.Item label='榜单类型'>
+            <Input placeholder='请输入……'/>
+          </Form.Item>
+          <Form.Item label='地区'>
+            <Input placeholder='请输入……'/>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
   );
 }
