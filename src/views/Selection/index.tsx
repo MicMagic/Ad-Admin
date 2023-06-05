deb1b1f8a07892d4d591fbe17015a05259c614d8
MagicHacker <zxmagichacker@gmail.com>
2023-06-05 22:50:47 +0800

    添加下拉框

--- src/views/Selection/index.tsx
+++ src/views/Selection/index.tsx
@@ -1,17 +1,48 @@
 import React from 'react';
-import { Form, Input } from 'antd';
+import { Form, Input, Select } from 'antd';
 import './index.scss';
 
+const rankList = [
+  {
+    label: '全部',
+    value: 'allList'
+  },
+  {
+    label: '免费榜',
+    value: 'freeList'
+  },
+  {
+    label: '付费榜',
+    value: 'payList'
+  },
+  {
+    label: '畅销榜',
+    value: 'sellWellList'
+  }
+];
+const device = [
+  {
+    label: 'iPhone',
+    value: 'iPhone'
+  },
+  {
+    label: 'iPad',
+    value: 'iPad'
+  }
+];
 export default function Selection() {
   return (
     <div className='selection-wrapper'>
       <div className='selection-header'>
         <Form layout='inline'>
           <Form.Item label='榜单类型'>
-            <Input placeholder='请输入……'/>
+            <Select defaultValue='allList' style={{ width: 80 }} options={rankList}/>
           </Form.Item>
           <Form.Item label='地区'>
-            <Input placeholder='请输入……'/>
+            
+          </Form.Item>
+          <Form.Item label='设备'>
+            <Select defaultValue='iPhone' style={{ width: 90 }} options={device}/>
           </Form.Item>
         </Form>
       </div>
